3c801027458110811bd40ca4940cbce42759046f
xFN10x <mathdudex09@gmail.com>
2026-09-15 20:41:59 -0300

    actually use yyjson

--- src/game_detection.c
+++ src/game_detection.c
@@ -1,17 +1,31 @@
 #include <SDL3/SDL.h>
 #include <string.h>
+#include <yyjson/yyjson.h>
 
-const char *GD_GameIdsJson;
+yyjson_doc *GD_GameIdsJson;
 
-void loadIds() {
-    size_t size = 0;
-    char* buf = (char*)SDL_LoadFile("assets/games.json", &size);
-    if (buf == NULL) {
+char *getGameName(char *id)
+{
+    return yyjson_get_str(yyjson_obj_get(yyjson_doc_get_root(GD_GameIdsJson), id));
+}
+
+void loadIds()
+{
+    SDL_Log("Reading game ids...");
+    size_t size;
+    void *buf = SDL_LoadFile("assets/games.json", &size);
+    if (buf == NULL)
+    {
         SDL_Log("Failed to load game ids: %s", SDL_GetError());
+        SDL_free(buf);
+        return;
     }
 
-    SDL_Log("Loaded: %d bytes", size);
-    
+    SDL_Log("Loaded: %zu bytes", size);
+    GD_GameIdsJson = yyjson_read(buf, size, 0);
+    SDL_Log("Loaded json.");
 
-    GD_GameIdsJson = buf;
+    SDL_free(buf);
+
+    SDL_Log(getGameName("SLUS-00067"));
 }
--- src/game_detection.h
+++ src/game_detection.h
@@ -2,5 +2,6 @@
 #define GD_H
 extern const char *GD_GameIdsJson;
 
+char* getGameName(const char* id);
 void loadIds();
 #endif
